4c7f0d6fddbfa81b4962195d3416dc8175526ad1
Meshrif <meshrif@Meshrifs-Mac-Pro.local>
2021-02-17 05:31:01 +0200

    edit chatScreen

--- lib/screens/chatScreen.dart
+++ lib/screens/chatScreen.dart
@@ -1,10 +1,59 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class ChatScreen extends StatelessWidget {
+import '../widgets/chatScreen/lowerBody/usersBody.dart';
+import '../providers/authProv.dart';
+import '../providers/usersProv.dart';
+import '../widgets/chatScreen/upperBody/callUserBar.dart';
+import '../widgets/chatScreen/upperBody/chatAndFriendsButtons.dart';
+import '../widgets/chatScreen/lowerBody/lastmessages/messagesBub.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    try {
+      Provider.of<UsersProv>(context, listen: false).setUsersData();
+    } catch (e) {
+      print(e);
+    }
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final s = MediaQuery.of(context).size;
+    final chUser = Provider.of<UsersProv>(context).chUser;
+    final userId = Provider.of<AuthProv>(context).userId;
     return Scaffold(
-      body: Container()
+      body: Column(
+        children: [
+          Container(
+            color: Colors.grey[300],
+            height: Platform.isAndroid ? s.height * 0.19 : s.height * 0.15,
+            padding: EdgeInsets.only(top: 43, left: 20, right: 20),
+            child: CallUserBar(
+              icon: Icons.search,
+              ontap: () {},
+              specificUser:
+                  Provider.of<UsersProv>(context).findUserById(userId),
+            ),
+          ),
+          Container(
+            height: s.height * 0.07,
+            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            child: ChatAndFriendsButtons(),
+          ),
+          !chUser
+              ? Expanded(child: MessagesBub())
+              : Expanded(child: UsersBody())
+        ],
+      ),
     );
   }
 }
