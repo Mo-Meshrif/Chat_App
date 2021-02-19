c099d357fb476f52fdebdf47b3b3831af0bf829e
Meshrif <meshrif@Meshrifs-Mac-Pro.local>
2021-02-19 09:39:53 +0200

    upload search widget

--- lib/widgets/cutomSearch.dart
+++ lib/widgets/cutomSearch.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 class CustomSearch extends SearchDelegate<User> {
   @override
   List<Widget> buildActions(BuildContext context) {
-    return [IconButton(icon: Icon(Icons.clear), onPressed: () => query = '')];
+    return null;
   }
 
   @override
@@ -26,35 +26,46 @@ class CustomSearch extends SearchDelegate<User> {
     List<User> users = Provider.of<UsersProv>(context).users;
     final fitlteredUsers =
         users.where((user) => user.userName.startsWith(query)).toList();
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        query.isEmpty
-            ? Padding(
-                padding: const EdgeInsets.only(top: 15, left: 18, bottom: 10),
-                child: Text('SUGGESTED'),
+    return fitlteredUsers.isNotEmpty
+        ? Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              query.isEmpty
+                  ? Padding(
+                      padding:
+                          const EdgeInsets.only(top: 15, left: 18, bottom: 10),
+                      child: Text('SUGGESTED'),
+                    )
+                  : Padding(padding: EdgeInsets.only(bottom: 10)),
+              Expanded(
+                child: ListView.builder(
+                  itemCount:
+                      query.isEmpty ? users.length : fitlteredUsers.length,
+                  itemBuilder: (context, i) => ListTile(
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.indigo[50],
+                      radius: 25,
+                      backgroundImage: NetworkImage(query.isEmpty
+                          ? users[i].imageUrl
+                          : fitlteredUsers[i].imageUrl),
+                    ),
+                    title: Text(
+                        '${query.isEmpty ? users[i].userName : fitlteredUsers[i].userName}'),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (_) => MessageScreen(
+                                friendId: query.isEmpty
+                                    ? users[i].userId
+                                    : fitlteredUsers[i].userId,
+                              )));
+                    },
+                  ),
+                ),
               )
-            : Padding(padding: EdgeInsets.only(bottom: 10)),
-        fitlteredUsers.isNotEmpty?Expanded(
-          child: ListView.builder(
-            itemCount:query.isEmpty? users.length:fitlteredUsers.length,
-            itemBuilder: (context, i) => ListTile(
-              leading: CircleAvatar(
-                backgroundColor: Colors.indigo[50],
-                radius: 25,
-                backgroundImage: NetworkImage(query.isEmpty?users[i].imageUrl:fitlteredUsers[i].imageUrl),
-              ),
-              title: Text('${query.isEmpty?users[i].userName:fitlteredUsers[i].userName}'),
-              onTap: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (_) => MessageScreen(
-                          friendId:query.isEmpty? users[i].userId:fitlteredUsers[i].userId,
-                        )));
-              },
-            ),
-          ),
-        ):Center(child: Text('There is no user !'),)
-      ],
-    );
+            ],
+          )
+        : Center(
+            child: Text('There is no user !'),
+          );
   }
 }
